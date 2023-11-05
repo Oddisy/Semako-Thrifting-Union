2c36779b6cc781d0e10d52c45629236c09d46c3c
Oddisy <odunayoogunleye200@gmail.com>
2023-11-05 17:31:52 +0100

    updated

--- src/containers/dashboard/AccountBalance/AccountBalance.jsx
+++ src/containers/dashboard/AccountBalance/AccountBalance.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./AccountBalance.css";
 import {AccountBalanceApi} from "../../../services/apiService";
-import Card from "../../../components/card/Card";
+import Card from "../../../components/card/card";
 
 function AccountBalance() {
 	return (
